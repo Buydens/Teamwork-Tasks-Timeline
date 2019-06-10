c8502f278e05e458f8291e60089fd4b2d14a17bd
Budyens <sam.buydens@protonmail.com>
2019-06-10 21:31:08 +0200

    Initial file and folder structure.

---
+++ src/components/Task.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="task">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="timeline">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timeline',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
